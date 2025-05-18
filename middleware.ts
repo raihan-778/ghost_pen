f21b1e3edf45d4c7835f49b87cb8d4ca012ddfc0
Raihan Uddin <raihanarif778@gmail.com>
2025-05-18 17:31:00 +0600

    middleware modified

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse, NextRequest } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
-
   const url = request.nextUrl;
+
+  if (
+    (token && url.pathname.startsWith("/sign-in")) ||
+    url.pathname.startsWith("/sign-up") ||
+    url.pathname.startsWith("/") ||
+    url.pathname.startsWith("/verify")
+  ) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
   return NextResponse.redirect(new URL("/home", request.url));
 }
 
