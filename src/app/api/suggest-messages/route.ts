5344f1f36d6e4de5b2793812df76d8155a233475
raihan-778 <raihanarif778@gmail.com>
2025-05-20 22:43:52 +0600

    suggest-msg route wiht ai intigration completed

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,6 +1,6 @@
 import { openai } from "@ai-sdk/openai";
 
-import { streamText } from "ai";
+import { generateText, streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 
@@ -8,12 +8,24 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    // const openai = new OpenAI({
+    //   apiKey: process.env.OPENAI_API_KEY,
+    // });
 
     const result = streamText({
-      model: openai("gpt-4o"),
-      messages,
+      model: openai("gpt-4o"), // Replacing instruct model with chat-style model
+      maxTokens: 400,
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
     });
+
     return result.toDataStreamResponse();
   } catch (error) {
     console.error("Error in suggest-messages", error);
